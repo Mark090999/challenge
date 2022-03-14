69db61f10c80dc9538f92960ecb5dfcbf63dc1f8
Mark <ccmarco090999@gmail.com>
2022-03-14 10:24:05 -0500

    Add seeder for customers

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('customers')->insert([
+            'fullname' => 'Marco Castillo',
+            'identification' => '1105078735',
+            'address' => 'Avenida Naciones Unidas',
+        ]);
+    }
+}
