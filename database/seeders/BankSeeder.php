6aad0f8b029b2c7dc9016475205746e0a3eebe4a
Mark <ccmarco090999@gmail.com>
2022-03-14 10:35:51 -0500

    Add seeder for banks

---
+++ database/seeders/BankSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BankSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('banks')->insert([
+            'name' => 'Banco Pichincha C.A',
+            'address' => 'Avenida Amazonas',
+            'swift_code' => '11112233444'
+        ]);
+
+        DB::table('banks')->insert([
+            'name' => 'Banco de Guayaquil',
+            'address' => 'Avenida Amazonas',
+            'swift_code' => '22225533444'
+        ]);
+    }
+}
